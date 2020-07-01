837a814249ed1a8ad491272b55639c2ee2f4de41
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-01 17:54:12 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -22,6 +22,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
         #your key/value pairs here
