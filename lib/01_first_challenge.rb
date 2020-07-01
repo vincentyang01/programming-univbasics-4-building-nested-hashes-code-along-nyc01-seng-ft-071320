8bfc5cefc28e13197648dcc877c9e2800ee7b006
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-01 17:49:49 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,11 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {},
+     :montague => {
+       :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+     },
     :capulet => {}
    }
 end
